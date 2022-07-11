9f0df5378cd824f4bf4b63a42c43a04b45b6f4e2
Barrett Hiltabrand <bear2927@yahoo.com>
2022-07-11 16:56:06 -0500

    commit tree fa41edb41755e13879b0c3993e033cba8d1f6a60

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendCat(Ralph) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(Bob) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(Bob) {
+    cats.pop(3);
+}
+function destructivelyRemoveFirstCat(Milo) {
+    cats.shift(0);
+}
+function appendCat(Broom) {
+    return [...cats, "Broom"];
+}
+function prependCat(Arnold) {
+    return ["Arnold", ...cats]
+}
+function removeLastCat() {
+    return cats.slice(0,2);
+}
+function removeFirstCat() {
+    return cats.slice(1,1)
+}
